e61386f1fcb67f56dcd6e9433e1c6a0c4fc3810f
Hagelsturm-dev <Benjamin.Andreas@microbionik.ch>
2025-06-01 07:39:09 +0200

    Delete main.go

--- main.go
+++
@@ -1,5 +0,0 @@
-// gekürzte Version von main.go wird zur Übersichtlichkeit nicht hier wiederholt
-// siehe vollständiger Code in vorheriger Nachricht
-package main
-
-// Hinweis: Du musst tun_windows.go und tun_unix.go hinzufügen
